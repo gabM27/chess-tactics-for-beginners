b3ba14b10f594af01b589609c7f9fda65503a580
Gabriele <gabriele.27.magazzu@gmail.com>
2023-04-28 13:45:39 +0200

    update

--- chessTacticsForBeginners.m
+++ chessTacticsForBeginners.m
@@ -4,22 +4,28 @@
 dataset = Import["C:\\Users\\gabri\\Documents\\Universit\[AGrave]\\Matematica computazionale\\chess-tactics-for-beginners\\test.csv","Dataset","HeaderLines"->1];
 board = ImportString["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "FEN"];
 
-generateNewChessBoard[]:= (
+generateNewChessBoard[]:= Module[ {num,fen,best},
 num = RandomInteger[{1,Length[dataset]}];
 fen = dataset[num,"fen"];
 best = dataset[num,"best"];
 ImportString[fen,"FEN"]
-)
-
+]
 
 (* Inserimento del nome da input utente *)
-nomeUtente = InputString[]
-
+nomeUtente = InputString[];
 
 newChessboard = Button["Nuova scacchiera", board=generateNewChessBoard[]];
 clearChessboard = Button["Pulisci scacchiera", board=ImportString["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "FEN"]];
 newChessboard clearChessboard
+nomeUtente "sta giocando!"
 Dynamic@board
+
+(*
+TODO: 
+ - ricordarsi del seed della posizione appena giocata --> scrivere su file txt stringa fen 
+	e successivamente creare menu a tendina per rigiocare la posizione salvata.
+ - muovere i pezzi tramite GUI e/o tramite input esterni (menu a tendina, etc.)
+*)
 
 
 
