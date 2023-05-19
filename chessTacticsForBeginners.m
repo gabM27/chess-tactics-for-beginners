8fa01f21a62f7306591a4c66e1ec1d61e1c8ca39
AndreaAccornero <78299965+AndreaAccornero@users.noreply.github.com>
2023-05-19 16:46:13 +0200

    Update chessTacticsForBeginners.m
    Aggiunta stampe dinamiche
    
    Co-Authored-By: andreabianchi8 <98221763+andreabianchi8@users.noreply.github.com>
    Co-Authored-By: Gabriele <95940596+gabM27@users.noreply.github.com>

--- chessTacticsForBeginners.m
+++ chessTacticsForBeginners.m
@@ -19,67 +19,61 @@ problems = Import["dataset.zip","*.txt"][[1]];
 MakePGNfiles[problems];
 
 
-whoIsPlaying;
 filepgn;
 pgntosplit;
 moveToCheck;
+whoIsPlaying = "";
+endgame = "";
 correctMove;
+correctMovetoShow = "";
 
-(* Crea la nuova scacchiera*)
-generateNewChessBoard[]:= Module[ {randomNum, board},
-(* So che i PGN files totali sono 11715*)
-randomNum=RandomInteger[{1,11715}];
-filepgn = PGNfile[randomNum]["PGN"];
-correctMove=Last[filepgn];
-correctMove = StringDrop[correctMove,-1]; (* droppo il "#" finale*)
-board=PGNconvert[filepgn];
-Chess[ShowBoard->board,Interact->True];
-If[StringMatchQ[PGNfile[randomNum]["Result"], "1-0"], whoIsPlaying = "mossa al BIANCO, trova lo scacco matto", whoIsPlaying = "mossa al NERO, trova lo scacco matto"];
-Move[MoveFromPGN[#][[1]]]&/@ Drop[board, Length[Movelist]-1];
-] 
-
-(* verifico che la mossa fatta equivalga a quella corretta
-TODO:
-1. sistemare stampa solo una volta e cancellazione stampa precedente.
-2. creare pulsante rigioca la stessa partita (possibilmente salvare per dopo) 
-*)
-checkMove[]:= Module[{pgntosplit,delimitatori,lista,var,len, moveToCheck},
-pgntosplit = PGN // Dynamic;
-delimitatori ={" " , ", "};
-lista = StringSplit[pgntosplit[[1]],delimitatori];
-len = Dimensions[lista];
-moveToCheck = Last[lista];
-Print["Correct: " correctMove <> " moveToCheck: " moveToCheck];
-Print[lista];
-If[StringMatchQ[moveToCheck,correctMove], endgame = "MOSSA CORRETTA, BRAVO!",endgame = "hai sbagliato, riprova o visualizza la soluzione :("];
-Print[endgame];
-Chess[ShowBoard->board,Interact->False];
+generateNewChessBoard[] := Module[{randomNum, board},
+  randomNum = RandomInteger[{1, 11715}];
+  filepgn = PGNfile[randomNum]["PGN"];
+  correctMove = Last[filepgn];
+  correctMove = StringDrop[correctMove, -1];
+  board = PGNconvert[filepgn];
+  Chess[ShowBoard -> board, Interact -> True];
+  If[StringMatchQ[PGNfile[randomNum]["Result"], "1-0"],
+    whoIsPlaying = "mossa al BIANCO, trova lo scacco matto",
+    whoIsPlaying = "mossa al NERO, trova lo scacco matto"];
+  Move[MoveFromPGN[#][[1]]] & /@ Drop[board, Length[Movelist] - 1];
 ]
 
-(* Inserimento del nome da input utente
-Controllo che la stringa non sia vuota o che non contenga solo whitespace *)
+checkMove[] := Module[{pgntosplit, delimitatori, lista, len, moveToCheck},
+  pgntosplit = PGN // Dynamic;
+  delimitatori = {" ", ", "};
+  lista = StringSplit[pgntosplit[[1]], delimitatori];
+  len = Dimensions[lista];
+  moveToCheck = Last[lista];
+  (*Print["Correct: " <> correctMove <> " moveToCheck: " <> moveToCheck];
+  Print[lista];*)
+  If[StringMatchQ[moveToCheck, correctMove],
+    endgame = "MOSSA CORRETTA, BRAVO!",
+    endgame = "hai sbagliato, riprova o visualizza la soluzione :("];
+  Chess[ShowBoard -> board, Interact -> False];
+]
+
 While[True,
- nomeUtente = InputString["Inserisci il tuo nome:"];
- If[!StringMatchQ[StringTrim[nomeUtente]][""], Break[]];
- ]
- (*Nel nome vengono rimpossi i whitespace *)
- StringReplace[nomeUtente," "-> ""] "sta giocando!"
+  nomeUtente = InputString["Inserisci il tuo nome:"];
+  If[! StringMatchQ[StringTrim[nomeUtente]][""], Break[]];
+]
+
+StringReplace[nomeUtente, " " -> ""] <> "sta giocando!"
 
 board = Startposition;
-Chess[ShowBoard->Interactive]
-newBoardBtn=Button["Nuova scacchiera", board=generateNewChessBoard[]];
-backBtn=Button["Back",Move[MoveFromPGN[filepgn[[Length[Movelist]-1]]][[1]]]]; (*non funziona ancora*)
-restartBtn=Button["Restart",Chess[ShowBoard->Startposition,Interact->True]];
-checkBtn=Button["Verifica mossa", checkMove[]];
-showSolutionBtn=Button["Mostra soluzione", Print["La mossa corretta \[EGrave] " <> correctMove]];
-(*TODO: capire perch\[EGrave] stampa prima showSolutionBtn degli altri*)
-newBoardBtn restartBtn checkBtn showSolutionBtn backBtn
+Chess[ShowBoard -> Interactive]
+newBoardBtn = Button["Nuova scacchiera", board = generateNewChessBoard[]];
+backBtn = Button["Back", Move[MoveFromPGN[filepgn[[Length[Movelist] - 1]]][[1]]]];
+restartBtn = Button["Restart", whoIsPlaying = ""; endgame = ""; correctMovetoShow =""; correctMove=""; Chess[ShowBoard -> Startposition, Interact -> True]];
+checkBtn = Button["Verifica mossa", checkMove[]];
+showSolutionBtn = Button["Mostra soluzione", correctMovetoShow = "La mossa corretta \[EGrave] " <> correctMove];
 
-(*stringa che mostra chi deve giocare*)
+Column[{newBoardBtn, restartBtn, checkBtn, showSolutionBtn, backBtn}]
+
 Dynamic@whoIsPlaying
-(*Lista di mosse della partita --> dalla quale prendere ultima mossa per confronto*)
-Dynamic@filepgn
-
+Dynamic@endgame
+Dynamic@correctMovetoShow
 
 
 
